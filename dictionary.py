56c4b73ef02b55cdf8114067c534c10cbc4ac55a
Jonnyto007 <jonnyto@outlook.com>
2020-12-15 18:58:51 -0800

    This is code to investigate dictionary.py file

---
+++ dictionary.py
@@ -0,0 +1,46 @@
+def new_dictionary():
+	dict={'Wei':2,'Jonny':7,'Abhi':3}
+	print(dict1)
+	dict2=dict1(dict1)
+	dict(dict2)
+	dict3={'wei':[2,1,3],'Jonny':[6,7,8],'Abhi':[3,4,5]}
+	print(dict3)
+#	dict4={[2,1,3]:'wei,[6,7,8]:'Jonny',[9,4,5]:'Abhi'}
+#	print(dict4)
+	dict5={'class1':dict3}
+	print(dict5)
+
+def dict_add():
+	dict1={'Wei':2,'Jonny':7,'Abhi':3}
+	dict1('Bhakti')=10
+	print(dict1)
+	dict2={'Kevin':11,'Wei':2'Jonny':7,'Abhi':3}
+	print(dict2.update(dict1))
+	dict1['Abhi']=8
+	print(dict1)
+
+def dict_access():
+	dict1={'Wei',:2,'Jonny:7,'Abhi:3}
+	print(dic1['Jonny'])
+	print(dict1.get(2)
+	print(dict1.get('Abhi'))
+	for keys in dict1:
+		print(keys,dict1[keys])
+
+
+def dict_delete():
+	dict1={wei':2, Jonny':7, Abhi':3}
+	del dict1['Jonny']
+	print(dict1)
+
+
+def pict_pass(**dict2):
+	print(dict2)
+		
+
+
+if __name__=='__main__':
+	dict_add()
+	dict1={wei':2, Jonny':7, Abhi':3}
+	dict_pass(name='Abhi',experience=3, gender='Male')
+

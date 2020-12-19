d15ca6a491c22b3b18020eab03ca734464b7eb45
WeiYuan1996 <weiyuanduke@gmail.com>
2020-12-18 21:04:25 -0500

    Revert "Square/cube root program"
    This reverts commit 62189adaafc3223fa91cd8dfee8ab96f96279c91.

--- lambda_square_cube.py
+++
@@ -1,6 +0,0 @@
-if __name__ == '__main__':
-	num = int(input("Enter a number: "))
-	squ_num = lambda x: x ** 2	
-	print("Square of ",num, ": %s"%squ_num(num))
-	cube_num = lambda y: y * y * y
-	print("Cube of ",num, ": %s"%cube_num(num))

00980d3561ff633b4b5c3df51cc084be0e8a9bb6
bhaktimp02 <bhaktimp02@gmail.com>
2020-12-21 21:09:26 -0500

    Recursive function

---
+++ recursive_function.py
@@ -0,0 +1,17 @@
+#Factorial 
+#3! = 3*2*1 = 6
+
+
+
+def factorial(a):
+	if a == 0:
+		return 1
+	else:
+		return x*factorail(x-1)
+
+	print(factorial(a))
+
+if __name__ == '__main__':
+	a = int(input("Enter a number to get a factorial: ")
+	factorial(a)
+	

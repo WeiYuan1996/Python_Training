46a3909e2dc5d3990d74b8bb367ce3cd2688276a
bhaktimp02 <bhaktimp02@gmail.com>
2020-12-21 22:16:34 -0500

    Update recursive_function.py

--- recursive_function.py
+++ recursive_function.py
@@ -15,3 +15,21 @@ if __name__ == '__main__':
 	a = int(input("Enter a number to get a factorial: ")
 	factorial(a)
 	
+
+
+#Fibonacci Number:
+#1,1,2,3,5,8,13,21
+
+
+def fib(b):
+        if b <= 1:
+                return b
+        else:
+                return fib(b-1) + fib(b-2)
+
+
+if __name__ == '__main__':
+        b = 9 #int(input("Enter a number to get Fibonacci: "))
+        print(fib(b))
+~                                                                                                                                            
+~                         

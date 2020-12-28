ca5dfabb3e83cea8b5179a5b7542e1f3b163cc76
Abhi <abhi.niki90@gmail.com>
2020-12-27 19:01:54 -0700

    New Changes

--- credit.py
+++ credit.py
@@ -8,14 +8,6 @@ class credit_card:
         #self._blnc_limit=None
         #self._cvv=None      
 
-#Parameterized constructor1
-    def __init__(self,cust,bank,accnt,blnc,blnc_limit,cvv):
-        self._cust=cust
-        self._bank=bank
-        self._accnt=accnt
-        self._blnc=blnc
-        self._blnc_limit=blnc_limit
-        self._cvv=cvv
 
 #Parameterized constructor2
     def __init__(self,cvv,cust='Bhakti',bank='Chase',accnt='23456',blnc=3549.09,blnc_limit=4000):
@@ -25,6 +17,17 @@ class credit_card:
         self._blnc=blnc
         self._blnc_limit=blnc_limit
         self._cvv=cvv 
+        print("Constructor 2")
+
+#Parameterized constructor1
+    def __init__(self,cust,bank,accnt,blnc,blnc_limit,cvv):
+        self._cust=cust
+        self._bank=bank
+        self._accnt=accnt
+        self._blnc=blnc
+        self._blnc_limit=blnc_limit
+        self._cvv=cvv
+        print("Constructor 1")
 
 #Getter Methods(starts with GET_<name>)
     def get_cust(self):
@@ -48,6 +51,13 @@ class child_credit_card(credit_card):
     def __init__(self,cust,bank,accnt,blnc,blnc_limit,cvv,int_per):
 #Initialize the base class from the child class
         super().__init__(cust,bank,accnt,blnc,blnc_limit,cvv)
+        #self._cust=cust
+        #self._bank=bank
+        #self._accnt=accnt
+        #self._blnc=blnc
+        #self._blnc_limit=blnc_limit
+        #self._cvv=cvv
+
         self._int_per=int_per
 
     def get_int_per(self):
@@ -77,6 +87,7 @@ if __name__ == '__main__':
 #Object creation for child class
     obj_child_class=child_credit_card('Jonny','Chase','12345',3456.67,5000,765,'10%')
     print(obj_child_class.get_int_per())
+    print(obj_child_class.get_blnc())
     a=int(input("Enter the deposit amount: "))
     obj_child_class.cal_chrg(a)
     print(obj_child_class.get_blnc())

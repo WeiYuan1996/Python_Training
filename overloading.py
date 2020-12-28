470601492c61dd2592294fc3b4204f2939f606fc
gxzpython <gxzpython@gmail.com>
2020-12-27 22:32:49 -0600

    Create overloading.py

---
+++ overloading.py
@@ -0,0 +1,23 @@
+class OverLoad:
+    def __init__(self,a):
+        self._a=a
+    def __add__(self,second):
+        return self._a*second._a
+    def __len__(self):
+        return(len(self._a)**2)
+
+if __name__=='__main__':
+    x= int(input("enter numberx: "))
+
+    y= int(input("enter numbery: "))
+    z= int(input("enter numbery: "))
+    w=input('enter a string:')
+    objOver= OverLoad(x)
+    objLoad=OverLoad(y)
+    objOver2=OverLoad(z)
+    objOver3= OverLoad(w)
+    print(objOver+objLoad)
+    print(objOver+objLoad+objOver2)
+    print(len(
+
+

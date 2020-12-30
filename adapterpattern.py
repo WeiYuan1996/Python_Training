272cefb224b2e3bd953af9c505c715444c13b30c
gxzpython <gxzpython@gmail.com>
2020-12-29 22:00:30 -0600

    Update adapterpattern.py

--- adapterpattern.py
+++ adapterpattern.py
@@ -26,6 +26,31 @@ class ListStack:
         else:
             return self._data[-1]
 
-    
+    def getSelf(self):
+        return self._data
 
-if __name__ =='__main__':
+if __name__ =="__main__":
+    a = ListStack()
+    p = input('please input a string you like with brackets "(){}[]": ')
+    for i in p:
+        if i in ['(','{','[']:
+            a.push(i)
+    print(a.getSelf())
+
+    for i in p:
+        if i in [')','}',']']:
+            if a.top() =='(':
+                a.pop()
+            elif a.top() =='{':
+                a.pop()
+            elif a.top()=='[':
+                a.pop()
+
+    print(a.getSelf())
+    if a.isEmpty():
+        print('Correct')
+    else:
+        print('Incorrect')
+
+
+
